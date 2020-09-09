3ca4775c4bf4c77542712e8bdd0a37b97fb22601
Amit <amitsingh880266@gmail.com>
2020-09-09 22:46:49 +0530

    basic design

--- includes/header.php
+++ includes/header.php
@@ -9,4 +9,4 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <title>Document</title>
 </head>
-<body>
+<body class="bg-light">
--- index.php
+++ index.php
@@ -1,8 +1,32 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/navbar.php'); ?>
 
-<h1>Amit</h1>
-<h1>Himu</h1>
-<h1>ayush</h1>
-<h1>shalu</h1>
+<div class="container">
+    <div class="m-2">
+        <h1 class="">Jagannath International Management School</h1>
+    </div>
+    <div class="row">
+        <div class="col-lg-3 p-1">
+            <div class="card bg-info" >
+                <img class="card-img-top rounded-circle p-3" height="130px" width="130px"  src="images/img1.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Ayush</h5>
+                    <p class="card-text"></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6">
+            <div>
+                <p>Post</p>
+                
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <h1>Amit</h1>
+            <h1>Himu</h1>
+            <h1>ayush</h1>
+            <h1>shalu</h1>  
+        </div>
+    </div>
+</div>
 <?php require_once('includes/footer.php'); ?>
