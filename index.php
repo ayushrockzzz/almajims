c313687184d3197e52343b14cf9d233692fb1cef
Amit <amitsingh880266@gmail.com>
2020-09-09 22:11:44 +0530

    test

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require_once('includes/header.php'); ?>
 <?php require_once('includes/navbar.php'); ?>
 
-
+<h1>Amit</h1>
+<h1>Himu</h1>
+<h1>ayush</h1>
 <?php require_once('includes/footer.php'); ?>
