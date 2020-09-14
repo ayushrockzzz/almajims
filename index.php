6dfe08b6986bb6f8cf81b118b46b757fb0851efa
Amit <amitsingh880266@gmail.com>
2020-09-14 22:17:28 +0530

    modified dashboard on 14th september

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 
 <div class="container">
     <div class="m-2">
-        <h1 class="">Jagannath International Management School</h1>
+        <div class="col-xl-12 col-lg-12 col-sm-12">
+            <h3 class="">Jagannath International Management School</h3>
+        </div>
     </div>
-    <div class="row">
-        <div class="col-lg-3 p-1">
+    <div class="row col-sm-12 ">
+        <div class="col-xl-3 col-lg-3 col-md-3 col-xm-3 col-sm-3 p-1 shadow">
             <div class="card bg-info" >
                 <img class="card-img-top rounded-circle p-3" height="130px" width="130px"  src="images/img1.png" alt="Card image cap">
                 <div class="card-body">
@@ -15,18 +17,37 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-6">
+        <div class="col-xl-5 col-lg-5 col-md-5 col-xm-5 col-sm-5 shadow ">
             <div>
-                <p>Post</p>
-                
+                <h3>Post</h3>
+                <p> adding Some sample tex</p>
+                <div class="col-lg-12 shadow">
+                    <div class="row">
+                        <div class="col-lg-6 ">
+                            <h3>Hello</h3>
+                        </div>
+                        <div class="col-lg-6 ">
+                            <h3>Hlleoow</h3>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="col-lg-3">
-            <h1>Amit</h1>
-            <h1>Himu</h1>
-            <h1>ayush</h1>
-            <h1>shalu</h1>  
+        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xm-4 shadow ">
+            <h3>Connection Suggestion</h3>
+            <div class="col-lg-12 mt-5 p-2 shadow">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <p>Get Your friends aboard</p>
+                    </div>
+                    <div class="col-lg-6">
+                        <button class="btn btn-primary btn-sm">Invite People To Join</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<br><br><br>
 <?php require_once('includes/footer.php'); ?>
