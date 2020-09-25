ef4c06e93a575b55241dc30fe6d3f3fb578ae421
Amit <amitsingh880266@gmail.com>
2020-09-25 21:46:31 +0530

    created css file

--- index.php
+++ index.php
@@ -4,24 +4,27 @@
 <div class="container">
     <div class="m-2">
         <div class="col-xl-12 col-lg-12 col-sm-12">
-            <h3 class="">Jagannath International Management School</h3>
+            <h3 class="mainHeadLine">Jagannath International Management School</h3>
         </div>
     </div>
     <div class="row col-sm-12 ">
-        <div class="col-xl-3 col-lg-3 col-md-3 col-xm-3 col-sm-3 p-1 shadow">
-            <div class="" style="background-color: #e3f2fd" >
-                <img class="card-img-top rounded-circle p-3" height="130px" width="130px"  src="images/img1.png" alt="Card image cap">
+        <div class="col-xl-3 col-lg-3 col-md-3 col-xm-3 col-sm-3 p-1 ">
+            <div class="profilesection" style="" >
+                <div class="profileImageIndex">
+                    <img class="card-img-top  p-3" src="images/img1.png" alt="Card image cap">
+                </div>
+                <hr>
                 <div class="card-body">
                     <h5 class="card-title">Ayush</h5>
                     <p class="card-text"></p>
                 </div>
             </div>
         </div>
-        <div class="col-xl-5 col-lg-5 col-md-5 col-xm-5 col-sm-5 shadow ">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-xm-6 col-sm-6  ">
             <div>
                 <h3>Post</h3>
                 <p> adding Some sample tex</p>
-                <div class="col-lg-12 shadow">
+                <div class="col-lg-12 ">
                     <div class="row">
                         <div class="col-lg-6 ">
                             <h3>Hello</h3>
@@ -33,18 +36,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-xm-4 shadow ">
-            <h3>Connection Suggestion</h3>
-            <div class="col-lg-12 mt-5 p-2 shadow">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <p>Get Your friends aboard</p>
-                    </div>
-                    <div class="col-lg-6">
-                        <button class="btn btn-primary btn-sm">Invite People To Join</button>
-                    </div>
-                </div>
-            </div>
+        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xm-3  ">
+            
         </div>
     </div>
 </div>
